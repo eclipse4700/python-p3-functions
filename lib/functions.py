0754b87a7446a84be0571e44227fc83a1ba60ea4
Isaiah Kantor <isaiahkantor23@gmail.com>
2024-07-15 16:53:31 -0400

    Finished Today

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    pass 
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(20,20))
+
 
 def halve(number):
-    pass
+    if type(number) != int and type(number) != float:
+        return None
+
+    return number / 2
